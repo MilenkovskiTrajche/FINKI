e8a818f80efa03bcf3310b813028ef60ac2ccd19
MilenkovskiTrajche <milenkovskitm@gmail.com>
2025-11-26 09:37:31 +0100

    missing commit

---
+++ VebFinki/lab/src/main/java/mk/ukim/finki/wp/lab/controller/DishController.java
@@ -0,0 +1,90 @@
+package mk.ukim.finki.wp.lab.controller;
+
+import mk.ukim.finki.wp.lab.model.Chef;
+import mk.ukim.finki.wp.lab.model.Dish;
+import mk.ukim.finki.wp.lab.service.ChefService;
+import mk.ukim.finki.wp.lab.service.DishService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/dishes")
+public class DishController {
+
+    private final DishService dishService;
+    private final ChefService chefService;
+
+    public DishController(DishService dishService, ChefService chefService) {
+        this.dishService = dishService;
+        this.chefService = chefService;
+    }
+
+    @GetMapping
+    public String getDishesPage(@RequestParam(required = false) String error, Model model) {
+        model.addAttribute("dishes", dishService.listDishes());
+        model.addAttribute("error", error);
+        return "listDishes";
+    }
+
+    @PostMapping("/add")
+    public String saveDish(@RequestParam String dishId,
+                           @RequestParam String name,
+                           @RequestParam String cuisine,
+                           @RequestParam int preparationTime) {
+        dishService.create(dishId, name, cuisine, preparationTime);
+        return "redirect:/dishes";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editDish(@PathVariable Long id,
+                           @RequestParam String dishId,
+                           @RequestParam String name,
+                           @RequestParam String cuisine,
+                           @RequestParam int preparationTime) {
+        dishService.update(id, dishId, name, cuisine, preparationTime);
+        return "redirect:/dishes";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteDish(@PathVariable Long id) {
+        dishService.delete(id);
+        return "redirect:/dishes";
+    }
+
+
+    @GetMapping("/dish-form")
+    public String getAddDishPage(Model model) {
+        model.addAttribute("dish", new Dish());
+        return "dish-form";
+    }
+
+    @GetMapping("/dish-form/{id}")
+    public String getEditDishForm(@PathVariable Long id, Model model) {
+        Dish dish = dishService.findById(id);
+        if (dish == null) {
+            return "redirect:/dishes?error=DishNotFound";
+        }
+        model.addAttribute("dish", dish);
+        return "dish-form";
+    }
+
+    @GetMapping("/add-to-chef")
+    public String addDishToChef(@RequestParam Long chefId,
+                                @RequestParam String dishId,
+                                Model model) {
+        chefService.addDishToChef(chefId, dishId);
+        Chef chef = chefService.findById(chefId);
+        model.addAttribute("chef", chef);
+        model.addAttribute("dishes", chef.getDishes());
+
+        return "chefDetails";
+    }
+
+    @PostMapping("/add-to-chef")
+    public String addDishToChefPost(@RequestParam Long chefId,
+                                    @RequestParam String dishId) {
+        chefService.addDishToChef(chefId, dishId);
+        return "redirect:/dishes/add-to-chef?chefId=" + chefId + "n&dishId=" + dishId;
+    }
+}
